c755ef46a8aeb057841c7864d74528ba81ac5586
Dylan Tobia <djtobia@gmail.com>
2022-08-02 12:11:34 -0700

    Added a footer with copyright and design info

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-row class="pl-5 bg-dark-blue text-light-blue mb-0" align="center">
+    <v-col cols="3">© Copyright Dodgeball Eugene {{ year }}</v-col>
+    <v-spacer />
+    <v-col  cols="3" lg="1"  xl="1">Designed by <a href="http://www.dylantobia.com" target="_blank">Dylan Tobia</a></v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+  const year = new Date().getFullYear();
+</script>
