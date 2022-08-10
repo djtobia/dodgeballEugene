ac805d4ad01fb3cc65f639a1a98473d7e305191a
Dylan Tobia <djtobia@gmail.com>
2022-08-10 10:54:58 -0700

    Moved social links to their own component, added red banner for urgent messages

--- src/App.vue
+++ src/App.vue
@@ -7,41 +7,15 @@
         <router-view />
       </v-container>
     </v-main>
-    <v-container class="text-light-blue">
-      <v-row align-content="center">
-        <v-col cols="6">
-          <v-row class="text-center pa-0 ma-0">
-            <v-col cols="12" class="pa-0">
-              <a href="https://www.facebook.com/groups/dodgeballeugene" target="_blank">
-                <v-img src="images/facebook.png" alt="facebook logo" max-height="50" />
-              </a>
-            </v-col>
-            <v-col cols="12" class="pa-0">
-              Join us on Facebook!
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col cols="6">
-          <v-row class="text-center pa-0 ma-0">
-            <v-col cols="12" class="pa-0">
-              <a href="https://www.meetup.com/dodgeball-eugene/" target="_blank">
-                <v-img src="images/meetup.png" alt="meetup logo" max-height="50" />
-              </a>
-            </v-col>
-            <v-col cols="12" class="pa-0">
-              Join us on Meetup!
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
+    <SocialLinks />
     <BottomNav />
   </v-app>
 </template>
 
 <script setup lang="ts">
 import MainNav from "@/components/MainNav.vue";
-import BottomNav from "./components/BottomNav.vue";
+import SocialLinks from "@/components/SocialLinks.vue";
+import BottomNav from "@/components/BottomNav.vue";
 import { onMounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import { initializeApp } from "firebase/app";
