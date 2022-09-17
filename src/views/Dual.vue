e8d0a137cd4ebc6bf6531aa917a9982f4087d308
Dylan Tobia <djtobia@gmail.com>
2022-09-16 23:12:47 -0700

    Added dual registration link

--- src/views/Dual.vue
+++ src/views/Dual.vue
@@ -6,9 +6,17 @@
       <p class="font-weight-bold pb-4">Our annual Dodgeball Unity and Love Tournament is back!</p>
     </div>
     <div id="when" class="text-h5">When: November 12th</div>
-    <div id="more-info" class="text-h6">More information and registration coming soon</div>
+    <div id="more-info" class="text-h6">7" Foam, draft style tournament!</div>
     <div id="event-page" class="text-h6">
-      View the event <a href="https://www.facebook.com/events/1972130449642622" target="_blank">here</a>
+      <a href="https://www.facebook.com/events/1972130449642622" target="_blank">View the event page </a>
+      <br><br>
+      Registration is now open!
+      <br><br>
+      <a class="font-weight-bold"
+        href="https://docs.google.com/forms/d/e/1FAIpQLScdAH_wHGbWeThFYBEt2F4gDufZLKi42od5c_5AqjA_F80e2g/viewform?fbclid=IwAR1QoHqm0-7VBPn3fiIbv7dwdBpcPGda0itTZsOV5CUqMwr_RDQTfslislE"
+        target="_blank">
+          Register here!
+      </a>
     </div>
   </v-container>
 </template>
