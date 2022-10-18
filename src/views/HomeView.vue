7340891e3978eb2c974cf22b2d56c10e41b2fecb
Dylan Tobia <djtobia@gmail.com>
2022-10-17 23:15:00 -0700

    Added DUAL logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,15 +27,6 @@
           </div>
           <p class="pa-2 pb-4">For more information, view our <a href="https://www.facebook.com/Dodgeball-Eugene-110959956928427/" target="_blank">Facebook</a> group, or our <a href="https://www.meetup.com/dodgeball-eugene/" target="_blank">Meetup</a>!</p>
         </div>
-            <!-- <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2866.1088944645935!2d-123.07612578424957!3d44.081106332672334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54c0e21a354334b9%3A0x57984ccb32c51d08!2sSheldon%20Community%20Center!5e0!3m2!1sen!2sus!4v1659129344618!5m2!1sen!2sus"
-            :width="mobile ? '300' : '600'"
-            :height="mobile ? '300' : '450'"
-            style="border:0;"
-            allowfullscreen="false"
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade">
-                    </iframe> -->
         </v-col>
         <v-spacer />
       </v-row>
@@ -79,8 +70,3 @@
   </v-row>
   <v-img src="/images/d-eugene-transparent.png" class="pt-10"></v-img>
 </template>
-
-<script setup lang="ts">
-import { useDisplay } from 'vuetify';
-const { mobile } = useDisplay();
-</script>
