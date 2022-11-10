ddba41e4868fef1ea0196583e341c082e1eeb469
Dylan Tobia <djtobia@gmail.com>
2022-11-09 22:50:24 -0800

    Updated rules for DUAL

--- src/components/rules/Outs.vue
+++ src/components/rules/Outs.vue
@@ -1,38 +1,52 @@
 <template>
   <ul class="text-left pl-5">
-    <li>If you get hit, you’re out. Easy right? Well, there are tricky some grey areas.</li>
-    <li>A player is considered out if they are contacted by a live ball thrown by the opposite team and it becomes dead.</li>
-    <li>Clothing: Uniforms and accessories are considered part of a player’s body.
+    <li>
+      If you get hit, you’re out. Easy right? Well, there are tricky some grey
+      areas.
+    </li>
+    <li>
+      A player is considered out if they are contacted by a live ball thrown by
+      the opposite team and it becomes dead.
+    </li>
+    <li>
+      Clothing: Uniforms and accessories are considered part of a player’s body.
       <ul class="pl-5">
         <li>
-          This includes hats. If you are wearing a hat and the brim gets hit by a ball that would normally not strike the players head, this is an out. For headshot clarification, see the “Headshots” section.
+          This includes hats. If you are wearing a hat and the brim gets hit by
+          a ball that would normally not strike the players head, this is an
+          out. For headshot clarification, see the “Headshots” section.
         </li>
       </ul>
     </li>
     <li>
       Hit etiquette: If a player is hit, they may pass balls under their control
-      to their teammates, raise their arm to signal that they are out, and
-      leave the court immediately without disrupting play.
+      to their teammates, raise their arm to signal that they are out, and leave
+      the court immediately without disrupting play.
     </li>
     <li>
       Once a player is out, if they make contact with a ball not under their
-      possession to pass to teammates that ball will be considered dead
-      and given to the opposing team.
-    </li>
-    <li>“If in doubt, you’re out!” If you are not sure if you were hit or safe, go out.</li>
-    <li>
-      A player is out only once the live ball has hit a dead surface. This
-      means that the player may throw or catch other balls until the original
-      ball is dead and they are officially out.
+      possession to pass to teammates that ball will be considered dead and
+      given to the opposing team.
     </li>
     <li>
-      Trap: When a live ball hits between a players foot and the ground at
-      the same time. This is considered an out.
+      “If in doubt, you’re out!” If you are not sure if you were hit or safe, go
+      out.
     </li>
     <li>
-      Too close to call: If a player takes a headshot while standing or in athletic pose and in the
-      same volley gets hit out the out is negated. If they take the headshot and catch a ball the catch
-      is still valid and thrower of the head shot is out.
+      A player is out only once the live ball has hit a dead surface, or
+      something the causes the live ball to die. This means that the player may
+      throw or catch other balls until the original ball is dead and they are
+      officially out.
+    </li>
+    <li>
+      Trap: When a live ball hits between a players foot and the ground at the
+      same time. This is considered an out.
+    </li>
+    <li>
+      Too close to call: If a player takes a headshot while standing or in
+      athletic pose and in the same volley gets hit out the out is negated. If
+      they take the headshot and catch a ball the catch is still valid and
+      thrower of the head shot is out.
     </li>
   </ul>
 </template>
