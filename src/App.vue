ba0316ff52dff2d4dde65da9ae1c234d1faa31d4
Dylan Tobia <djtobia@gmail.com>
2023-05-09 15:55:36 -0700

    Removed old env test

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,6 @@ onMounted(() => {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-  console.log(import.meta.env.VITE_TEST);
 });
 </script>
 <style>
